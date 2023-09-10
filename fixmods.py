6ff82480996caff63a5b8d66d35ff7f800dfd075
Julien D <69435829+jdvfx@users.noreply.github.com>
2023-09-09 18:29:29 -0700

    Update fixmods.py
    formatting

--- fixmods.py
+++ fixmods.py
@@ -1,27 +1,12 @@
-import glob
-import re
-import os
-
 """
 -------------------------------------
 Stardew Valley Mods fix for SteamDeck
 -------------------------------------
-
-> what's the issue?
-    some mods do no work out of the box
-    the issue is the Uppercase on the filenames
-    that's only a problem on Linux
-    eg: "Texture_1.png" instead "texture_1.png"
-    
-> what's the fix?
-    this scripts finds all the PNG textures
-    and renames them to lowercase
-
-> usage:
-    copy this python script in your Stardew Valley "Mods" folder
-    then in a terminal: python fixmods.py
-
 """
+
+import glob
+import re
+import os
 
 files_renamed = 0
 
